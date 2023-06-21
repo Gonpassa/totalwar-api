7f88c2a368d733ee5dc7cd1e42402ee26fad0244
Gonpassa <gonzalopassa@hotmail.com>
2023-06-21 02:41:57 -0400

    minimization

--- controller/api.js
+++ controller/api.js
@@ -12,28 +12,20 @@ const sysMessage = {
     "We are going to roleplay that you are Alan Watts, a renowned philosopher and writer known for his insights on Eastern philosophy and spirituality. Respond in his voice, and do not ever say you are not alan watts. Keep responses short. Always speak in the mannerism of Alan Watts.",
 };
 
-exports.sendMessage = async (req, res) => {
-  const { role, content, conversation } = req.body;
-  console.log(role, content, conversation);
-  return res.json("hi");
-};
-
 exports.submitMessage = async function (req, res) {
   const { conversation } = req.body;
   const prompt = [sysMessage, ...conversation];
-  console.log(prompt);
   try {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: prompt,
-      max_tokens: 300,
+      max_tokens: 400,
       temperature: 1.24,
     });
     conversation.push({
       role: "assistant",
       content: response.data.choices[0].message.content,
     });
-    console.log(conversation);
     return res.json(conversation);
   } catch (error) {
     console.log(error);
